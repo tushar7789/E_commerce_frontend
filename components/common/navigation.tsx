956b7e4b14e4907efc9b77b1ab0e59ed4ce1cdd2
tushar7789 <tusharjangid7@gmail.com>
2026-08-03 19:07:34 +0530

    completed searchbar

--- components/common/navigation.tsx
+++ components/common/navigation.tsx
@@ -7,10 +7,11 @@ import LogoImage from '@/public/static_images/logo.png'
 import CartImage from '@/public/static_images/cart.png'
 
 import ButtonComp from './button';
+import SearchBar from '../ui/search/SearchBar';
 
 function NavigationBar() {
     return (
-        <div className="h-12 w-full px-6 flex items-center">
+        <div className="h-14 w-full px-6 flex items-center bg-gray-700">
             <LogoComp />
             <SearchComp />
             <ProfileComp />
@@ -20,32 +21,30 @@ function NavigationBar() {
 
 function LogoComp() {
     return (
-        <div className='h-full w-2/12'>
-            <Link href={"/"}>
-                <Image src={LogoImage.src} height={45} width={45} alt="" />
-            </Link>
-        </div>
+        <Link className='h-full w-2/12 flex items-center justify-start' href={"/"}>
+            <Image src={LogoImage.src} height={45} width={45} alt="" />
+        </Link>
     );
 }
 
 function SearchComp() {
     return (
-        <div className='h-full w-8/12 flex justify-center items-center bg-red-500'>
-            SearchBar
+        <div className='h-full w-8/12 flex justify-center items-center'>
+            <SearchBar />
         </div>
     );
 }
 
 function ProfileComp() {
     return (
-        <div className='h-full w-3/12 flex items-end'>
-            <ButtonComp to={"/"} tailwindStyleString='h-full w-3/8 flex justify-center items-center bg-amber-700'>
-                <span className='h-full pr-3 flex justify-center items-center'>Login</span>
+        <div className='h-full w-3/12 py-[10px] flex justify-around items-center'>
+            <ButtonComp to={"/"} tailwindStyleString='h-full w-4/12 flex justify-center items-center border-0 rounded-[5px] bg-white hover:bg-amber-300'>
+                <span className='h-full flex justify-center items-center'>Login</span>
             </ButtonComp>
-            <ButtonComp to={"/"} tailwindStyleString='h-full w-3/8 flex justify-center items-center bg-amber-700'>
-                <span className='h-full pr-3 flex justify-center items-center'>More</span>
+            <ButtonComp to={"/"} tailwindStyleString='h-full w-4/10 flex justify-center items-center border-0 rounded-[5px] bg-white hover:bg-amber-300'>
+                <span className='h-full flex justify-center items-center'>More</span>
             </ButtonComp>
-            <ButtonComp to={"/"} tailwindStyleString='h-full w-2/8 flex justify-center items-center bg-amber-700'>
+            <ButtonComp to={"/"} tailwindStyleString='h-full w-2/12 flex justify-center items-center border-0 rounded-[5px] bg-white'>
                 <Image src={CartImage.src} height={20} width={20} alt="" />
             </ButtonComp>
         </div>
